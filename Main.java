2205a8f9bf066fe942c2d90d9f59d4ef19d1f470
Alok Kumar Maurya <alokkumarmaurya5556@gmail.com>
2022-11-28 22:36:04 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 class Main{
-public static void main(String [] asd){
-System.out.print("Hello");
-}
+    static void fn(){
+        System.out.println("Hello world");
+    }
+    
+    static void fn(int a){
+        System.out.println("Hello world" + a);
+    }
+    
+     static void fn(int a,int b){
+        System.out.println("Hello world" + a);
+        System.out.println("Hello world" + b);
+    }
+    
+    public static void main(String []asdd){
+        fn();
+        fn(8);
+        fn(2,7);
+    }
 }
